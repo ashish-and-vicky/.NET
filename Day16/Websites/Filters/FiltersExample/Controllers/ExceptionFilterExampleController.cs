93d3eb99cce98badb142c605539116f6c70f980a
Ashish Sharma <62541813+ashish-and-vicky@users.noreply.github.com>
2021-03-09 17:21:29 +0530

    Add files via upload

---
+++ Day16/Websites/Filters/FiltersExample/Controllers/ExceptionFilterExampleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FiltersExample.Controllers
+{
+    //[HandleError]----------when we give to show our custom error pages, this then it will find the page(Error.cshtml ViewBag) where this error page is there
+    public class ExceptionFilterExampleController : Controller
+    {
+        // GET: ExceptionFilterExample
+
+        //[HandleError]---------if there are multiple error pages and we want to run this method only then we mention this 
+        [MyErrorHandler] //this is used when we want to generate our own handled exception error class or when we want to generate our own additional exception handling code 
+        public ActionResult Index(int ID = 0)
+        {
+            //some code here
+            int i = 100;
+            i = i / ID;
+            return View();
+        }
+    }
+}
---
+++ Day16/Websites/Filters/FiltersExample/Controllers/ResultFilterExampleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FiltersExample.Controllers
+{
+    public class ResultFilterExampleController : Controller
+    {
+        // GET: ResultFilterExample
+        [OutputCache(Duration = 30, VaryByParam ="none")] //here a cache is stored in from the value which were stored from the last page visited---this is done for when the user if again visits will get the same value/credentials----VaryByParam ="none" means take cache same for all
+        public string Index()
+        {
+            return DateTime.Now.ToString("T"); //when we run this then the time will be cashed for 30 sec---if we visit within 30 sec the it will show previous visit time---after 30 sec it will change
+        }
+        [OutputCache(Duration = 30, VaryByParam ="id")] //if we visit the page with different id then it will show the same time(cached data)---therefore to generate different cache of different pages we write VaryByParam ="id"
+        public string Index2(int id= 0)
+        {
+            return DateTime.Now.ToString("T");
+        }
+    }
+}
