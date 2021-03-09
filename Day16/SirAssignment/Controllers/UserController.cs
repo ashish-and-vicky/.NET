eed332f6f0f2a98e02c72ff7916055297b965b9a
Ashish Sharma <62541813+ashish-and-vicky@users.noreply.github.com>
2021-03-09 16:07:47 +0530

    Add files via upload

---
+++ Day16/SirAssignment/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using ModelBindingAndDbCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+
+namespace SirAssignment.Controller
+{ 
+	public class UserController : Controller
+	{
+		public ActionReader Register()
+		{
+			
+		}
+
+
+	}
+}
