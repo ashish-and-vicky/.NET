72d9b7d673e37db7ba9299b245f41d0174cecab0
Ashish Sharma <62541813+ashish-and-vicky@users.noreply.github.com>
2021-03-09 17:51:05 +0530

    Add files via upload

---
+++ Day16/Websites/HtmlHelpersExample/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlHelpersExample.Models
+{
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string Name { get; set; }
+        public decimal Basic { get; set; }
+        public short DeptNo { get; set; }
+        public bool IsActive { get; set; }
+        public IEnumerable<SelectListItem> Departments { get; set; } //we have taken this because we want to display the Departments in a drop down (Employee has a Department as foreign key)---SelectListItem has a property with text and value
+                                                                     //this is called a view model class which has a collection of Employees and Departments and then pass boththe objects to view
+    }
+
+}
